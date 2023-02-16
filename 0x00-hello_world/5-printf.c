0ebd271dad530f4cf723c84d7b7fafe07e77ba9a
koech2 <iankoech30@gmai.com>
2023-02-16 15:09:52 -0800

    0x00. C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio>
+/**
+ * main - A program that prints aline using printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	pintf("with proper grammar, but the outcome is a piece of art, \n")
+	return (0);
+}
